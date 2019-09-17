8a197f47606e973d36a1e43f5f231058d94fa457
colettedcosta <44319285+colettedcosta@users.noreply.github.com>
2019-09-16 20:15:27 -0400

    Commit message to test git push

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Development/BasicTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Development/BasicTeleOp.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.TeleOp.Development;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.TeleOp.TeleOpMethods;
 
+//Define program in program registry
 @TeleOp(name = "Basic TeleOp", group = "Development")
 
 //Basic TeleOp Program to test just the wheels
