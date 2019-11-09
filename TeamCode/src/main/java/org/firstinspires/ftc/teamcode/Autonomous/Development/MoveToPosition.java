63c31cb92361c364b44e7e6db13a26017fc498e9
Nicolas Bravo <nicobravo1@hotmail.com>
2019-11-09 11:26:40 -0500

    Finished MoveToPosition to move to a coordinate using the strafe drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Development/MoveToPosition.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Development/MoveToPosition.java
@@ -15,77 +15,61 @@ public class MoveToPosition extends AutoMethods {
     public void runOpMode() {
         initRobot();
 
-        ///DON"T DO THIS
-        ////INSTEAD DO THE THING WHERE IT GOES FOR Y FIRST THEN FOR X OR SOMETHING
-        ////LIKE ONE AFTER OTHER NOT BOTH AT ONCE
-
         double x = 0;
         double y = 0;
 
         double left = 0;
         double right = 0;
 
-        //go to:
-        double xFinal = 50*ENCIN;
-        double yFinal = 50*ENCIN;
+        int xInch = 50;
+        int yInch = 50;
 
+        //go to place*inches:
+        double xFinal = xInch*ENCIN;
+        double yFinal = yInch*ENCIN;
+
+        float rot = 0;
+
+        leftFrontDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightFrontDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         leftBackDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rightBackDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        double degreeFinal = 0;
-        if (xFinal != Math.abs(xFinal) && yFinal != Math.abs(yFinal)) {
-            degreeFinal = Math.toDegrees(Math.atan((yFinal-y)/(xFinal-x))) + 180;
-        }
-        else if (xFinal != Math.abs(xFinal)) {
-            degreeFinal = Math.toDegrees(Math.atan((yFinal-y)/(xFinal-x))) + 180;
-        }
-        else if (yFinal != Math.abs(yFinal)) {
-            degreeFinal = Math.toDegrees(Math.atan((yFinal-y)/(xFinal-x)));
-        }
-        else {
-            degreeFinal = Math.toDegrees(Math.atan((yFinal-y)/(xFinal-x)));
-        }
-
-        orientation = gyro.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZXY, AngleUnit.DEGREES);
-        float rot = orientation.firstAngle+180;
-        while(degreeFinal < rot) {
-            leftBackDrive.setPower(0.5);
-            rightBackDrive.setPower(-0.5);
-            telemetry.addData("Gyro+180", rot);
-            telemetry.update();
+        while(Math.abs(x)<Math.abs(xFinal)) {
             orientation = gyro.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZXY, AngleUnit.DEGREES);
             rot = orientation.firstAngle+180;
-        }
-        while (degreeFinal > rot) {
-            leftBackDrive.setPower(0.3);
-            rightBackDrive.setPower(-0.3);
-            telemetry.addData("Gyro+180", rot);
-            telemetry.update();
-            orientation = gyro.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZXY, AngleUnit.DEGREES);
-            rot = orientation.firstAngle+180;
-        }
 
-        leftBackDrive.setPower(0);
-        rightBackDrive.setPower(0);
-
-        while(xFinal > x && yFinal > y) {
-
-            orientation = gyro.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZXY, AngleUnit.DEGREES);
-            rot = orientation.firstAngle+180;
-            telemetry.addData("Gyro+180", rot);
-
-            leftBackDrive.setPower(Range.clip(rot/degreeFinal, -1, 1));
-            rightBackDrive.setPower(Range.clip(degreeFinal/rot, -1, 1));
+            leftFrontDrive.setPower(0.5 * (xInch/Math.abs(xInch)));
+            leftBackDrive.setPower(0.5 * (xInch/Math.abs(xInch)));
+            rightFrontDrive.setPower(0.5 * (xInch/Math.abs(xInch)));
+            rightBackDrive.setPower(0.5 * (xInch/Math.abs(xInch)));
 
             double rad = Math.toRadians(rot);
-            x += -1*Math.sin(rad)*(((leftBackDrive.getCurrentPosition()-left) + (rightBackDrive.getCurrentPosition()-right)) / 2.0);
+            x += -1*Math.sin(rad)*(((((leftBackDrive.getCurrentPosition()+leftFrontDrive.getCurrentPosition())/2.0)-left) + (((rightBackDrive.getCurrentPosition()+rightFrontDrive.getCurrentPosition())/2.0)-right)) / 2.0);
             y += Math.cos(rad)*(((leftBackDrive.getCurrentPosition()-left) + (rightBackDrive.getCurrentPosition()-right)) / 2.0);
-            left = leftBackDrive.getCurrentPosition();
-            right = rightBackDrive.getCurrentPosition();
-
-            telemetry.update();
+            left = (leftBackDrive.getCurrentPosition() + leftFrontDrive.getCurrentPosition()) / 2.0;
+            right = (rightBackDrive.getCurrentPosition() + rightFrontDrive.getCurrentPosition()) / 2.0;
         }
+
+        while(Math.abs(y)<Math.abs(yFinal)) {
+            orientation = gyro.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZXY, AngleUnit.DEGREES);
+            rot = orientation.firstAngle+180;
+
+            leftFrontDrive.setPower(0.5 * (yInch/Math.abs(yInch)));
+            leftBackDrive.setPower(-0.5 * (yInch/Math.abs(yInch)));
+            rightFrontDrive.setPower(-0.5 * (yInch/Math.abs(yInch)));
+            rightBackDrive.setPower(0.5 * (yInch/Math.abs(yInch)));
+
+            double rad = Math.toRadians(rot);
+            x += -1*Math.sin(rad)*(((((leftBackDrive.getCurrentPosition()+leftFrontDrive.getCurrentPosition())/2.0)-left) + (((rightBackDrive.getCurrentPosition()+rightFrontDrive.getCurrentPosition())/2.0)-right)) / 2.0);
+            y += Math.cos(rad)*(((leftBackDrive.getCurrentPosition()-left) + (rightBackDrive.getCurrentPosition()-right)) / 2.0);
+            left = (leftBackDrive.getCurrentPosition() + leftFrontDrive.getCurrentPosition()) / 2.0;
+            right = (rightBackDrive.getCurrentPosition() + rightFrontDrive.getCurrentPosition()) / 2.0;
+        }
+
+        leftFrontDrive.setPower(0);
         leftBackDrive.setPower(0);
+        rightFrontDrive.setPower(0);
         rightBackDrive.setPower(0);
     }
 }
