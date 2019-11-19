cf17e7d7c58c8f3b8fd63c77d33196665c79e3af
Nicolas Bravo <nicobravo1@hotmail.com>
2019-11-18 21:33:21 -0500

    Updates to TeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Master/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Master/TeleOp.java
@@ -16,28 +16,28 @@ public class TeleOp extends TeleOpMethods {
             if (stage == 0) {
                 blockServo.setPosition(0.7);
                 liftMotor.setPower(0.2);
-                liftMotor.setTargetPosition(-3500);
+                liftMotor.setTargetPosition(-3100);
                 stage++;
             }
             else if (stage == 1) {
-                if (liftMotor.getCurrentPosition() < -3000) {
+                if (liftMotor.getCurrentPosition() < liftPosition-3000) {
                     blockServo.setPosition(0);
                     liftMotor.setPower(0.3);
                     liftMotor.setTargetPosition(0);
                     stage++;
                 }
-                else if (liftMotor.getCurrentPosition() < -200) {
+                else if (liftMotor.getCurrentPosition() < liftPosition-200) {
                     liftMotor.setPower(1);
                 }
             }
             else if (stage == 2) {
-                if (liftMotor.getCurrentPosition() > -2800) {
+                if (liftMotor.getCurrentPosition() > liftPosition-2800) {
                     liftMotor.setPower(1);
                     stage++;
                 }
             }
             else {
-                if (liftMotor.getCurrentPosition() > -25) {
+                if (liftMotor.getCurrentPosition() > liftPosition-25) {
                     initRun = false;
                 }
             }
@@ -49,10 +49,10 @@ public class TeleOp extends TeleOpMethods {
             //MAKE THE ARM THING ONE BUTTON!
             moveArm();
 
-            if (g2_a) {
+            if (gamepad1.right_bumper || g2_a) {
                 intakeLeft.setPower(1);
                 intakeRight.setPower(1);
-            } else if (g2_b) {
+            } else if (gamepad1.left_bumper || g2_b) {
                 intakeLeft.setPower(-1);
                 intakeRight.setPower(-1);
             } else {
@@ -60,19 +60,28 @@ public class TeleOp extends TeleOpMethods {
                 intakeRight.setPower(0);
             }
 
-            if (g2_dpad_up && liftMotor.getCurrentPosition() > -3900) {
-                liftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-                liftMotor.setPower(-1);
+            if (gamepad1.b) {
+                slowmode = false;
             }
-            else if (g2_dpad_down && liftMotor.getCurrentPosition() < 0) {
+            else if (gamepad1.a){
+                slowmode = true;
+            }
+
+            if (g2_leftstick_y < 0 && liftMotor.getCurrentPosition() > liftPosition-3700) {
                 liftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-                liftMotor.setPower(1);
+                liftMotor.setPower(g2_leftstick_y / 1.5);
+            }
+            else if (g2_leftstick_y > 0 && liftMotor.getCurrentPosition() < liftPosition) {
+                liftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                liftMotor.setPower(g2_leftstick_y / 1.5);
             }
             else {
                 liftMotor.setPower(1);
                 liftMotor.setTargetPosition(liftMotor.getCurrentPosition());
                 liftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             }
+
+            telemetry.addData("lift", liftPosition - liftMotor.getCurrentPosition());
 
 
             /*liftMotor.setPower(1);
