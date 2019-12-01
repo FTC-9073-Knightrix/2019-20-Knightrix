6574ccaa5a22d35d758f091bad8118070c3eae6a
Ignacio Bravo <ibravo@hotmail.com>
2019-12-01 08:31:28 -0500

    Saves Gyro_pos file on the phone.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Development/AutoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Development/AutoTest.java
@@ -4,8 +4,15 @@ package org.firstinspires.ftc.teamcode.Autonomous.Development;
 //Import the dependencies needed to run the program
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.util.ReadWriteFile;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.internal.system.AppUtil;
 import org.firstinspires.ftc.teamcode.Autonomous.AutoMethods;
+
+import java.io.File;
 
 //Add the program to the index of Autonomous programs
 @Disabled
@@ -23,6 +30,14 @@ public class AutoTest extends AutoMethods {
         //Wait for the driver to press start on the phone
         waitForStart();
 
+        //Records the current position into a Text File
+        orientation = gyro.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZXY, AngleUnit.DEGREES);
+        int StartingOrientation = (int) orientation.firstAngle;
+        String filename = "Gyro_pos.json";
+        File file = AppUtil.getInstance().getSettingsFile(filename);
+        ReadWriteFile.writeFile(file,StartingOrientation);
+
+        
         //Move forwards at half speed
         gyroMove(0, 0.4, 30000, 0);
     }
